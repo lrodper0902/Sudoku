46226d359c82a957fc08a5bfef4c10405ff33160
lrodper0902 <lorodper0902@g.edcuaand.es>
2023-12-12 05:37:17 +0100

    termiando

--- js/sudoku.js
+++ js/sudoku.js
@@ -1,16 +1,6 @@
 class Sudoku {
     constructor(mezclas = 30) {
         this.datos = [
-/*            1, 1, 1, 1, 1, 1, 1, 1, 1,
-            2, 2, 2, 2, 2, 2, 2, 2, 2,
-            3, 3, 3, 3, 3, 3, 3, 3, 3,
-            4, 4, 4, 4, 4, 4, 4, 4, 4,
-            5, 5, 5, 5, 5, 5, 5, 5, 5,
-            1, 2, 3, 4, 5, 6, 7, 8, 9,
-            1, 2, 3, 4, 5, 6, 7, 8, 9,
-            1, 2, 3, 4, 5, 6, 7, 8, 9,
-            1, 2, 3, 4, 5, 6, 7, 8, 9*/
-
             9,	2,	3,	8,	6,	1,	7,	4,	5,
             5,	4,	1,	2,	7,	9,	3,	8,	6,
             7,	6,	8,	4,	3,	5,	2,	9,	1,
@@ -23,6 +13,7 @@ class Sudoku {
         ];
         this.nuevo(mezclas);
     }
+
     intercambiaFila(i = 10) {
         switch (i) {
             case 0:
@@ -123,62 +114,120 @@ class Sudoku {
             this.intercambiaFila();
             this.intercambiaColumna();
         }
+        // Guardar una copia de los números antes de que se aplique el porcentaje
+        this.sudokuOriginal = [...this.datos];
     }
-
-    muestra(porcentaje=1) {
+    
+    muestra(porcentaje = 1) {
         for (let i = 0; i < 81; i++) {
-            document.getElementById('td' + i).innerText = this.datos[i] = Math.random()<porcentaje ? this.datos[i]:'';
+            const celd = document.getElementById('td' + i);
+            document.getElementById('td' + i).innerText = this.datos[i] = Math.random() < porcentaje ? this.datos[i] : '';
+            celd.style.color = 'green';
+            celd.style.fontWeight = 'bold';
         }
     }
 
-    estaResuelto() {
-        // debe devolver true o false
-    }
+    mostrarResultados() {
+        const sudokuResultados = [...this.datos];
+        for (let i = 0; i < 81; i++) {
+            const celd = document.getElementById('td' + i);
+            if (celd.innerText.trim() === '') {
+                celd.innerText = sudokuResultados[i];
+                celd.style.color = 'black'; // Cambia el color del texto para diferenciar los resultados
+            }
+        }
+    }    
 
-    
 }
 
+document.getElementById('rendirse').addEventListener('click', function() {
+    miSudoku.mostrarResultados();
+});
+
+function mostrarMensaje(mensaje) {
+    alert(mensaje);
+}
+
+        //Comprobamos las filas, las columnas y los minisudokus
+
+function verificarFilas() {
+    for (let fila = 1; fila <= 9; fila++) {
+        let numeros = [];
+        const filas = document.querySelectorAll('[data-fila="f' + fila + '"]');
+        
+        for (let i = 0; i < filas.length; i++) {
+            const numero = filas[i].innerText.trim();
+            if (numeros.includes(numero) || numero === '') {
+                //Miramos si hay algun numero repetido o esta vacío
+                return false; 
+            }
+            numeros.push(numero);
+        }
+    }
+    return true; 
+}
+
+function verificarColumnas() {
+    for (let columna = 1; columna <= 9; columna++) {
+        let numeros = [];
+        const columnas = document.querySelectorAll('[data-columna="c' + columna + '"]');
+        
+        for (let i = 0; i < columnas.length; i++) {
+            const numero = columnas[i].innerText.trim();
+            if (numeros.includes(numero) || numero === '') {
+                return false; 
+            }
+            numeros.push(numero);
+        }
+    }
+    return true; 
+}
+
+function verificarMiniSudokus() {
+    let bloques = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+    for (let bloque of bloques) {
+        let numeros = [];
+        const miniSudoku = document.querySelectorAll('[data-celda="' + bloque + '"]');
+        
+        for (let i = 0; i < miniSudoku.length; i++) {
+            const numero = miniSudoku[i].innerText.trim();
+            if (numeros.includes(numero) || numero === '') {
+                return false; 
+            }
+            numeros.push(numero);
+        }
+    }
+    return true; 
+}
+
+
 function establecerColor(id) {
+    // Si no esta selecionado quitamos el color
+    let todasLasCeldas = document.querySelectorAll('.cellnormal');
+    todasLasCeldas.forEach(function (celda) {
+        celda.style.backgroundColor = '#FFF';
+    });
+
+    //Obtenemos las celdas que estan seleccionadas
     let celdaSeleccionada = document.getElementById(id);
     let dataCelda = celdaSeleccionada.getAttribute('data-celda');
+    let filaCelda = celdaSeleccionada.getAttribute('data-fila');
+    let columnaCelda = celdaSeleccionada.getAttribute('data-columna');
+
+    
     let celdas = document.querySelectorAll('[data-celda="' + dataCelda + '"]');
     celdas.forEach(function (celda) {
         celda.style.backgroundColor = '#85FEBC'; //celeste
     });
 
-    let todasLasCeldas = document.querySelectorAll('.cellnormal');
-    todasLasCeldas.forEach(function (celda) {
-        if (celda.getAttribute('data-celda') !== dataCelda) {
-            celda.style.backgroundColor = '#FFF';
-        }
-    });
-
-    let filaSeleccionada = document.getElementById(id);
-    let filaCelda = filaSeleccionada.getAttribute('data-fila');
     let filas = document.querySelectorAll('[data-fila="' + filaCelda + '"]');
     filas.forEach(function (fila) {
         fila.style.backgroundColor = '#85FEBC'; //celeste
     });
 
-    let todasLasFilas = document.querySelectorAll('.cellnormal');
-    todasLasFilas.forEach(function (fila) {
-        if (fila.getAttribute('data-fila') !== filaCelda) {
-            fila.style.backgroundColor = '#FFF';
-        }
-    });
-
-    let columnaSeleccionada = document.getElementById(id);
-    let columnaCelda = columnaSeleccionada.getAttribute('data-columna');
     let columnas = document.querySelectorAll('[data-columna="' + columnaCelda + '"]');
     columnas.forEach(function (columna) {
         columna.style.backgroundColor = '#85FEBC'; //celeste
-    });
-
-    let todasLascolumna = document.querySelectorAll('.cellnormal');
-    todasLascolumna.forEach(function (columna) {
-        if (columna.getAttribute('data-columna') !== dataColumna) {
-            columna.style.backgroundColor = '#FFF';
-        }
     });
 }
 
@@ -198,28 +247,24 @@ function clickEnTabla(evento) {
         return;
     console.log("click en el id: " + evento.target.id);
     console.log("último foco en " + celdaUltimoFoco);
-    if (celdaUltimoFoco != -1) {
-        document.getElementById(celdaUltimoFoco).classList.remove("gamehighlighttd");
-    }
-    evento.target.classList.add("gamehighlighttd");
-    celdaUltimoFoco = evento.target.id;
 
     let celdas = document.querySelectorAll('.cellnormal');
     celdas.forEach(function (celda) {
         celda.addEventListener('click', function (event) {
-            // Cambiar color
+            // Cambia el color
             establecerColor(event.target.id);
         });
     });
 }
 
+const miSudoku = new Sudoku();
 document.getElementById('nuevoSudoku').addEventListener('click', nuevoSudoku);
 
-// document.getElementById(' BtnFacil').addEventListener('click',nuevoSudoku(0.75));
-// document.getElementById(' BtnMedio').addEventListener('click',nuevoSudoku(0.5));
-// document.getElementById(' BtnDificil').addEventListener('click',nuevoSudoku(0.35));
-
 document.getElementById('playtable').addEventListener('click', clickEnTabla);
+
+document.getElementById('comprobar').addEventListener('click', comprobar);
+
+            //Nivel
 
 document.getElementById('BtnFacil').addEventListener('click', function() {
     const miSudoku = new Sudoku();
@@ -231,6 +276,7 @@ document.getElementById('BtnMedio').addEventListener('click', function() {
     const miSudoku = new Sudoku();
     miSudoku.nuevo();
     miSudoku.muestra(0.5);
+
 });
 
 document.getElementById('BtnDificil').addEventListener('click', function() {
@@ -239,42 +285,142 @@ document.getElementById('BtnDificil').addEventListener('click', function() {
     miSudoku.muestra(0.35);
 });
 
-
+document.getElementById('rendirse').addEventListener('click', function() {
+    miSudoku.reiniciar();
+});
+                //Cambiar numero en las celdas seleccionadas
 document.addEventListener('DOMContentLoaded', function() {
-    // Guardamos todas las celdas del sudoku y las celdas con los nmeros del 1 al 9
-    const celdas = document.querySelectorAll('.cellnormal'); 
-    const valorCeldas = document.querySelectorAll('.btnSeleccionar');
-    let seleccionarCelda = null;
+    let celdaSeleccionada = null; // Variable para almacenar la celda seleccionada
 
+    const cells = document.querySelectorAll('td');
 
-    // Le añadimo el evento click y miramos si esta vacía para poder insertar uno d elos valores del 1 al 9
-    celdas.forEach(celda => {
-        celda.addEventListener('click', function(event) {
-            seleccionarCelda = event.target; //evento clic al selecionar una celda
-
-            if (seleccionarCelda.innerText.trim() === '') {
-                valorCeldas.forEach(valorCelda => {
-                    valorCelda.addEventListener('click', function() {
-                        const valorSelec = valorCelda.innerText;// Obtenemos el valor seleccionado
-                        seleccionarCelda.innerText = valorSelec;
-
-                        establecerColor(seleccionarCelda.id);// Cambiar el color de la celda seleccionada en el Sudoku
-                    });
-                });
-            } else {
-                seleccionarCelda = null;
-            }
+    // Asignar evento de click para seleccionar la celda
+    cells.forEach(celda => {
+        celda.addEventListener('click', function() {
+            celdaSeleccionada = this; // Almacena la celda seleccionada
         });
     });
 
-    function establecerColor(celda) {
+    //Numeros por teclado
+    document.addEventListener('keydown', function(event) {
+        const teclaPresionada = event.key;
+        const esNumero = !isNaN(parseInt(teclaPresionada));
+
+        if (celdaSeleccionada !== null && esNumero && parseInt(teclaPresionada) > 0 && parseInt(teclaPresionada) <= 9) {
+            celdaSeleccionada.innerText = teclaPresionada;
+            establecerColor(celdaSeleccionada.id); 
+        }
+    });
+        function establecerColor(celda) {
         const seleccionarCelda = document.getElementById(celda);
         seleccionarCelda.style.backgroundColor = '#B5FDA3'; //color más clarito
     }
 });
 
 
+const cells = document.querySelectorAll('td');
+const celdasMostrar = document.querySelectorAll('.btnSeleccionar');
+// cuando hagamos clic en una celda, guardamos en una variable todos los valores que tenga el mismo valor que data-fila
+// y lo guardamos en un array para luego mostrarlos
+
+for (let i = 0; i < cells.length; i++) {
+    cells[i].addEventListener('click', function() {
+        let fila = this.getAttribute('data-fila');
+        let columna = this.getAttribute('data-columna');
+        let miniSudoku = this.getAttribute('data-celda');
+        
+        // Obtener los números de la fila correspondiente
+        let arrNum = [];
+        let celdFila = document.querySelectorAll(`[data-fila="${fila}"]`);
+        for (let j = 0; j < celdFila.length; j++) {
+            const numero = celdFila[j].innerText.trim();
+            if (numero !== '') {
+                arrNum.push(parseInt(numero));
+            }
+        }
+
+        // Ahora hacemos lo mismo con la columna
+        let celColumna = document.querySelectorAll(`[data-columna="${columna}"]`);
+        for (let j = 0; j < celColumna.length; j++) {
+            const numero = celColumna[j].innerText.trim();
+            if (numero !== '') {
+                arrNum.push(parseInt(numero));
+            }
+        }
+
+        // Y por último el mini sudoku
+        let celMiniSudoku = document.querySelectorAll(`[data-celda="${miniSudoku}"]`);
+        for (let j = 0; j < celMiniSudoku.length; j++) {
+            const numero = celMiniSudoku[j].innerText.trim();
+            if (numero !== '') {
+                arrNum.push(parseInt(numero));
+            }
+        }
+
+        // Mostramos los valores
+        //Obtenemos los valores ordenados y sin repetidos
+        arrNum.sort(function (a, b) {
+            return a - b;
+        });
+          
+        const numMostrados = new Set(arrNum);
+        const mostrarNumeros = [];
+
+        for (let i = 1; i <=9; i++) {
+            if (!numMostrados.has(i)) {
+                mostrarNumeros.push(i);                
+            }
+        }
+
+        console.log(mostrarNumeros);
+
+        // Borrramos los valores para que no se vayan aculmulando
+        celdasMostrar.forEach(celda => {
+            celda.innerText = '';
+        });
+
+        // Mostrar los nuevos valores
+        let e = 0;  
+        mostrarNumeros.forEach(numero => {
+            if (e < celdasMostrar.length) {
+                celdasMostrar[e].innerText = numero;
+                e++;
+            }
+        });
+    });
+    const celdasSeleccionables = document.querySelectorAll('.btnSeleccionar');
+}
 
 
+function comprobar() {
+    //Vamos a mirar que todas las condiciones sean true
+    const sudokuValido = verificarFilas() && verificarColumnas() && verificarMiniSudokus();
+    const mensajeElemento = document.getElementById('mensajeSudoku');
+    mensajeElemento.style.color = 'black';
+    mensajeElemento.style.fontSize = '14px';
 
+    const celdas = document.querySelectorAll('td');
 
+    let sudokuIncompleto = false;
+
+    for (let i = 0; i < celdas.length; i++) {
+        if (celdas[i].innerText.trim() === '') {
+            sudokuIncompleto = true;
+            break;
+        }
+    }
+
+    if (sudokuIncompleto) {
+        mensajeElemento.textContent = 'El Sudoku está incompleto.';
+        //Mostramos el mensaje 3 segundos
+        setTimeout(() => {
+            mensajeElemento.textContent = '';
+        }, 3000);
+    } else {
+        if (sudokuValido) {
+            mostrarMensaje("Enhorabuena! ;)");
+        } else {
+            mostrarMensaje("Fallaste! Inténtalo de nuevo");
+        }
+    }
+}
